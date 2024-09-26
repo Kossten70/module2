3546be397dede8b724b7d37a416077662f0addcf
Konstantin <ref6512722@gmail.com>
2024-09-26 16:36:02 +0300

    01

---
+++ 01for.py
@@ -0,0 +1,13 @@
+# list_ = ['one', 'two', 'three']
+# for i in list_:
+#     if i == 'three':
+#         list_.remove(i)
+#
+# print(list_)
+
+list_ = ['one', 'two', 'three']
+for i in range(len(list_)):
+    print(list_[i]) # Кажый элемент списка
+
+
+
