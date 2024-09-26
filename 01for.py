4ff1195d84f7da31f8a3f4904a3a42b68c7060b2
Konstantin <ref6512722@gmail.com>
2024-09-26 17:21:03 +0300

    Таблица умножения

--- 01for.py
+++ 01for.py
@@ -5,9 +5,9 @@
 #
 # print(list_)
 
-list_ = ['one', 'two', 'three']
-for i in range(len(list_)):
-    print(list_[i]) # Кажый элемент списка
+# list_ = ['one', 'two', 'three']
+# for i in range(len(list_)):
+#     print(list_[i]) # Кажый элемент списка
 
 for i in range(1, 11): #start, stop, step
     for j in range(1, 11):
