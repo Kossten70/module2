eb8c6801a2bdbef4e913f48abc5010da901e1e59
Konstantin <ref6512722@gmail.com>
2024-10-07 20:57:21 +0300

    Правильное решение

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,11 +1,16 @@
-# num = int(input('Введите число из первого поля: '))
-first = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-ys = []
-y = 1
-for i in (first):
-    if y < i and i % y == 0:
-        ys.append(ys)
-        i = i + 1
-        print(ys)
+def find_password(n):
+    password = ''  # Пустая строка для хранения результата
+
+    # Перебираем все возможные пары чисел от 1 до n
+    for i in range(1, n):
+        for j in range(i + 1, n + 1): # Пределы числа J
+            if n % (i + j) == 0:  # Проверяем кратность числа n сумме пары
+                password += str(i) + str(
+                    j)  # Добавляем пару в строку результата
+
+    return password
 
 
+# Пример использования:
+n = int(input("Введите число от 3 до 20: "))
+print(f"Нужный пароль: {find_password(n)}")
