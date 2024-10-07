ded8e700374f6b11e0fc4062b7a920c431474fe0
Konstantin <ref6512722@gmail.com>
2024-10-07 18:23:32 +0300

    03

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,7 +1,11 @@
 # num = int(input('Введите число из первого поля: '))
 first = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-ys = ()
+ys = []
+y = 1
 for i in (first):
-    print(i)
-    i=i+1
+    if y < i and i % y == 0:
+        ys.append(ys)
+        i = i + 1
+        print(ys)
+
 
